c3e8791521e239b5ae7452a643afbe165513ddc6
swe-ashutosh <swe.ashutosh@gmail.com>
2025-06-17 14:38:55 +0530

    datatypes understand

---
+++ 01Basics/Notes.js
@@ -0,0 +1,15 @@
+console.log("hello javascript");
+
+/*
+
+const accountId = 144434
+accountId = 8684  its not allowed to reassign a constant variable
+
+"use strict";// treat all JS code as newer version.
+
+alert(3+3) its a node environment not browser environment.
+
+mdn and tc39 is use read documentaion   and ctrl + I is uset to copilot.
+
+
+*/
