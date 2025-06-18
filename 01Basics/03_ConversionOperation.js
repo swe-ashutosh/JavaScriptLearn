6d0f7e203b8b7fc6397f2ccfe193b584fe59e781
swe-ashutosh <swe.ashutosh@gmail.com>
2025-06-18 18:02:24 +0530

    conversion operation

---
+++ 01Basics/03_ConversionOperation.js
@@ -0,0 +1,12 @@
+// let score = "33adf"; // string
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber); // NaN (Not a Number)
+// console.log(typeof valueInNumber); 
+
+
+let isLoggedIn ;// boolean
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); 
